f296424002efc2ac7d398e4a33d8f17ada07a188
Kashif <kashiv.agnelite@gmail.com>
2015-05-18 16:19:48 -0300

    Testing static webpage with express

--- server.js
+++ server.js
@@ -3,9 +3,7 @@
 var express = require('express'); //Add express.
 var app = express(); // Express now ready to be used.  
 
-app.get('/', function(req, res) {  // '/' route to our index page
-	res.send("Hello World from server.js");  //testing to make sure server is running currectly
-});
+app.use(express.static(__dirname + "/public")); //static - html, javascript and CSS file. Telling the server to look for static files
 
 app.listen(3000);
 console.log("Server running on port 3000");
